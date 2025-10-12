426210d229ad4bca40d9dc4b645d7d238b66037a
Harsh Yadav <harshy9893@gmail.com>
2025-10-12 14:25:00 +0530

    add Toast component and integrate with App for better user notifications

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -6,24 +6,36 @@ import Add from './pages/Add.jsx'
 import List from './pages/List.jsx'
 import Orders from './pages/Orders.jsx'
 import Login from './components/Login.jsx'
+import { useEffect } from 'react'
+import { useToast,ToastContainer } from './components/Toast.jsx'
 
+
+export const backendUrl = import.meta.env.VITE_BACKEND_URL;
+export const currency = "₹";
 function App() {
-  const [token,setToken] =React.useState("");
+  const [token,setToken] =React.useState(localStorage.getItem("token") || "");
+  const toast  = useToast();
+
+  useEffect(()=>{
+    localStorage.setItem("token", token);
+  }, [token]);
 
   return (
     <div className='bg-gray-50 min-h-screen'>
+      <ToastContainer toasts={toast.toasts} removeToast={toast.removeToast} />
       {
-        !token ? <Login/>:
+        !token ? <Login setToken={setToken}/>:
         <>
-          <Navbar/>
+          <Navbar setToken={setToken}/>
           <hr />
           <div className='flex w-full'>
             <Sidebar/>
             <div className='w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base'>
               <Routes>
                 {/* Define your routes here */}
-                <Route path='/list' element={<List/>}/>
-                <Route path='/orders' element={<Orders/>}/>
+                <Route path='/add' element={<Add token={token} toast={toast}/>}/>
+                <Route path='/list' element={<List token={token} toast={toast}/>}/>
+                <Route path='/orders' element={<Orders token={token} toast={toast}/>}/>
               </Routes>
             </div>
           </div>
